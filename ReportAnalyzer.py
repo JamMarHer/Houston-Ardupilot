9fcbf21e46fffaf9956179c7bfa4a685e1b76005
Jam <jamarck96@gmail.com>
2017-06-25 19:24:34 -0400

    report file is now not opened twice

--- ReportAnalyzer.py
+++ ReportAnalyzer.py
@@ -8,13 +8,11 @@ class ReportAnalyzer(object):
         self.report = report
 
     def analyze(self):
-        with open("report.json") as file:
-            json_file = json.load(file)
         counts = {'PTP': {}, 'MPTP': {}, 'Extraction': {}}
         count = 0
-        for report in json_file['Reports']:
+        for report in self.report['Reports']:
             count += 1
-            counts[json_file['Reports'][report]['MissionType']][report] = json_file['Reports'][report]
+            counts[self.report['Reports'][report]['MissionType']][report] = self.report['Reports'][report]
 
         print 'PTP:        {}'.format(len(counts['PTP']))
         print 'MPTP:       {}'.format(len(counts['MPTP']))
